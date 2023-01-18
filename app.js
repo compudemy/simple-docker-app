318db21e388faf7f8665acd4654315e380da145f
Isaiah Daniel <isaiahdaniel203@gmail.com>
2023-01-18 07:17:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("Home");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`server running`));
